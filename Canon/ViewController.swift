6e2154ce34a5ce96ba810a539c471c77c8a99a04
Andrew Kealy <andrewkealy1@gmail.com>
2021-11-08 12:37:00 +0000

    First commit. This is a demo of ARKit and Reality Kit in iOS originally published here https://usdzshare.com/reality-kit/

--- Canon/ViewController.swift
+++ Canon/ViewController.swift
@@ -5,6 +5,8 @@
 //  Created by Andrew Kealy on 08/11/2021.
 //
 
+// The following demo is built from code and animations published on usdzshare.com
+
 import UIKit
 import RealityKit
 
@@ -15,10 +17,33 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Load the "Box" scene from the "Experience" Reality File
-        let boxAnchor = try! Experience.loadBox()
+        // Load the "Bang" scene from the "Experience" Reality File
+        let cannonAnim = try! Experience.loadBang()
+        
+        let anchor = AnchorEntity(plane: .horizontal, classification: .any, minimumBounds: [0.2, 0.2])
         
         // Add the box anchor to the scene
-        arView.scene.anchors.append(boxAnchor)
+        arView.scene.anchors.append(anchor)
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(onTap))
+        
+        arView.addGestureRecognizer(tapGesture)
+        
+        //Add the cannon animation to arView
+
+        arView.scene.anchors.append(cannonAnim)
+        
+    }
+    
+    @IBAction func onTap(_ sender: UITapGestureRecognizer) {
+        
+        let tapLocation = sender.location(in: arView)
+        
+        //Get the entity at the location we've tapped
+        
+        if let cannonFiring = arView.entity(at: tapLocation) {
+            print(cannonFiring.name)
+            print("Firing cannon")
+        }
     }
 }
