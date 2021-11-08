cb39fecfe6ab3640d17178348fc0d410690e2550
Andrew Kealy <andrewkealy1@gmail.com>
2021-11-08 11:45:19 +0000

    Initial Commit

---
+++ Canon/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Canon
+//
+//  Created by Andrew Kealy on 08/11/2021.
+//
+
+import UIKit
+import RealityKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var arView: ARView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+    }
+}
